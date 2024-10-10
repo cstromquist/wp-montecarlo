d4a5ec8e15171ead813ca729c1daf3fca79e9926
Chris Stromquist <chris.stromquist@gmail.com>
2024-10-10 00:16:15 -0700

    couple small style fixes

--- wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-hero/render.php
+++ wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-hero/render.php
@@ -25,16 +25,16 @@ $image               = !empty(get_field('image')) ? get_field('image') : '';
 ?>
 <div class="bg-gradient-to-b from-[#0073b4] to-[#005180]">
   <div class="flex min-h-[700px] max-w-screen-xl mx-auto px-4 items-center gap-2 pt-28 <?php echo esc_html($flex_hash[$alignment]); ?>">
-    <div class="flex flex-col w-7/12">
-      <h1 class="text-6xl text-white"><?php echo nl2br($heading); ?></h1>
-      <h2 class="text-xl font-normal text-white lg:pt-8 m-0"><?php echo nl2br($subheading); ?></h2>
+    <div class="flex flex-col w-7/12 pl-6">
+      <h1 class="text-6xl leading-[70px] text-white"><?php echo nl2br($heading); ?></h1>
+      <h2 class="text-xl font-normal text-white lg:pt-6 m-0"><?php echo nl2br($subheading); ?></h2>
       <div class="flex align-middle py-8 space-x-6 justify-<?php echo $justify_hash[$alignment] ?>">
         <a href="#" class="bg-[#eda600] px-8 py-2 text-white rounded-full text-[#1d4b67] hover:text-[white]">Take a product tour</a>
         <a href="#" class="border-[#eda600] border px-10 py-2 text-white hover:text-[#eda600] rounded-full">Request a demo</a>
       </div>
     </div>
     <?php if ($image && $alignment !== 'center') : ?>
-    <div class="w-5/12"><img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="w-full h-auto" /></div>
+    <div class="w-5/12 -mt-10"><img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="h-auto" /></div>
     <?php endif; ?>
   </div>
 </div>
