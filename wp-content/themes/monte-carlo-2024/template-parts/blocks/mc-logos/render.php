77d8842fcf83cf1a7824224506ac7f8eda24ec48
Chris Stromquist <chris.stromquist@gmail.com>
2024-10-10 23:05:02 -0700

    fix: add responsive for hero

--- wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-logos/render.php
+++ wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-logos/render.php
@@ -7,7 +7,7 @@
 
 $images = !empty(get_field('images')) ? get_field('images') : '';
 ?>
-<div class="mc-logos relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-10 lg:py-10 bg-white rounded-lg -mt-20 flex justify-around flex-wrap shadow-xl">
+<div class="mc-logos relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-10 lg:py-10 bg-white xl:rounded-lg md:-mt-20 flex justify-around flex-wrap shadow-xl">
   <?php foreach ($images as $image) : ?>
   <div class="w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-1/7 p-4">
     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="h-auto" />
