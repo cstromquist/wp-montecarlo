4052b56a553e7342def99d75b59e10b36bc6f79a
Chris Stromquist <chris.stromquist@gmail.com>
2024-10-10 23:34:13 -0700

    fix: alignment

--- wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-hero/render.php
+++ wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-hero/render.php
@@ -24,7 +24,7 @@ $image               = !empty(get_field('image')) ? get_field('image') : '';
 
 ?>
 <div class="bg-gradient-to-b from-[#0073b4] to-[#005180]">
-  <div class="flex lg:flex-row flex-col md:min-h-[800px] container mx-auto px-4 items-center gap-2 pt-36 lg:pt-16 <?php echo esc_html($flex_hash[$alignment]); ?>">
+  <div class="flex lg:<?php echo esc_html($flex_hash[$alignment]); ?> flex-col md:min-h-[800px] container mx-auto px-4 items-center gap-2 pt-36 lg:pt-16 <?php echo esc_html($flex_hash[$alignment]); ?>">
     <div class="flex flex-col lg:w-7/12 text-center lg:text-left pl-6">
       <h1 class="text-5xl mb-8 lg:mb-0 lg:text-6xl lg:leading-[70px] text-white"><?php echo nl2br($heading); ?></h1>
       <h2 class="text-[24px] font-sans font-normal text-white lg:pt-6 m-0"><?php echo nl2br($subheading); ?></h2>
@@ -33,7 +33,7 @@ $image               = !empty(get_field('image')) ? get_field('image') : '';
         <a href="#" class="text-xs sm:text-sm md:text-xl font-serif border-secondary border border-solid px-10 py-2 text-white hover:text-secondary rounded-full">Request a demo</a>
       </div>
     </div>
-    <?php if ($image && $alignment !== 'center') : ?>
+    <?php if ($image) : ?>
     <div class="w-5/12 md:-mt-10 lg:-ml-8 pb-10 lg:pb-0"><img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="h-auto" /></div>
     <?php endif; ?>
   </div>
