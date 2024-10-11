d1212987fecf9145debdb905cacfbe2ed86239e8
Chris Stromquist <chris.stromquist@gmail.com>
2024-10-10 23:05:16 -0700

    add theme colors to tailwind config

--- wp-content/themes/monte-carlo-2024/tailwind.config.js
+++ wp-content/themes/monte-carlo-2024/tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   content: ["./template-parts/**/*.{php,html,js}","./*.{php,html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'serif': ['Capito', 'serif'],
+      'sans': ['National', 'sans-serif'],
+    },
+    colors: {
+      'primary': '#1d4b67',
+      'secondary': '#eda600',
+      'gray': '#5b5b5b',
+      'white': '#fff',
+    },
   },
+  safelist: [
+    'lg:justify-start',
+    'cursor-pointer',
+  ],
   plugins: [],
   module: {},
   important: true,
