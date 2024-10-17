212e5a8090118fcd6b4c14e6387ad85f7fe033bd
Chris Stromquist <chris.stromquist@gmail.com>
2024-10-16 18:59:39 -0700

    fix: images in carousel

--- wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-carousel/render.php
+++ wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-carousel/render.php
@@ -12,13 +12,15 @@ $carousel_items = !empty(get_field('carousel_items')) ? get_field('carousel_item
   <div class="flex flex-row justify-between items-center inner-container">
     <div class="flex flex-col w-full md:w-1/2 text-primary [&>div>p]:text-gray md:pr-16">
       <?php foreach ($carousel_items as $i => $item) : ?>
-        <div class="flex flex-col carousel-item cursor-pointer relative p-4 <?php echo $i == 0 ? 'active' : ''; ?>" data-slide="<?php echo $i ?>">
-          <h4 class="text-2xl font-sans font-normal mb-2 mt-0"><?php echo esc_html($item['carousel_item_title']) ?></h4>
-          <p class="content text-lg"><?php echo esc_html($item['carousel_item_text']) ?></p>
+        <div class="flex flex-col carousel-item cursor-pointer relative <?php echo $i == 0 ? 'active' : ''; ?>" data-slide="<?php echo $i ?>">
+          <h4 class="text-2xl font-sans font-normal mb-2 mt-0 p-4"><?php echo esc_html($item['carousel_item_title']) ?></h4>
+          <p class="content text-lg px-4"><?php echo esc_html($item['carousel_item_text']) ?></p>
         </div>
       <?php endforeach; ?>
     </div>
     <div class="hidden md:block img flex flex-col w-1/2 relative transform-none">
-      <img data-slide="<?php echo $i ?>" src="<?php echo esc_url($item['carousel_image']['url']) ?>" alt="<?php echo esc_attr($item['carousel_item_image']['alt']) ?>" class="w-full" />
+      <?php foreach ($carousel_items as $i => $item) : ?>
+        <img data-slide="<?php echo $i ?>" src="<?php echo esc_url($item['carousel_image']['url']) ?>" alt="<?php echo esc_attr($item['carousel_item_image']['alt']) ?>" class="w-full" />
+      <?php endforeach; ?>
     </div>
   </div>
