895bc08a6013a50d9afca1986a1e079a8fd1c806
Chris Stromquist <chris.stromquist@gmail.com>
2024-10-18 22:09:12 -0700

    feat: add pinned nav buttons and styling plus mobile styling.

--- wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-pinned-nav/render.php
+++ wp-content/themes/monte-carlo-2024/template-parts/blocks/mc-pinned-nav/render.php
@@ -1,8 +1,17 @@
-<div class="left-1/2 -ml-[250px] z-50 mc-pinned-nav">
+<div class="sm:-ml-[275px] md:-ml-[325px] z-50 mc-pinned-nav left-0 sm:left-1/2 right-0">
   <!-- 3 column grid -->
-  <div class="grid grid-cols-3 w-[500px] border border-solid border-primary p-16 bg-white text-center">
-    <div class="bg-red-500">1</div>
-    <div class="bg-green-500">2</div>
-    <div class="bg-blue-500">3</div>
+  <div class="grid grid-cols-[2fr_2fr_4fr_4fr] items-center w-full sm:w-[550px] md:w-[650px] p-2 sm:p-6 bg-white text-center drop-shadow-lg sm:rounded-b-xl">
+    <div>
+      <img width="300" height="200" src="/wp-content/uploads/2023/06/mc-logo.svg" alt="Monte Carlo Data Logo">
+    </div>
+    <div>
+      <a href="https://getmontecarlo.com/signin" target="_blank" class="text-primary text-xs sm:text-sm md:text-lg font-serif underline underline-offset-4">Log in</a> 
+    </div>
+    <div>
+      <a href="#" data-lookbook-overlay-href="https://resources.montecarlodata.com/data-observability-product-tour" class="mc-btn-secondary text-xs sm:text-sm md:text-lg text-primary hover:underline px-2 sm:px-4 border-primary">Take a product tour</a>
+    </div>
+    <div class="bg-blue-500">
+      <a href="#" data-lookbook-overlay-href="https://resources.montecarlodata.com/data-observability-product-tour" class="mc-btn-primary text-xs sm:text-sm md:text-lg px-6 hover:underline">Request a demo</a>
+    </div>
   </div>
 </div>
